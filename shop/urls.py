98fdcea3da657a64b10cb32c3f722d7e763b974b
mohamad <netfars@gmail.com>
2015-06-30 17:44:31 +0430

    feat: Add Brand API

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from commerce.views import *
 from commerce.api.product import *
 from commerce.api.category import *
+from commerce.api.brand import *
 
 admin.autodiscover()
 
@@ -13,12 +14,10 @@ urlpatterns = patterns(
     url(r'^admin/files-widget/', include('topnotchdev.files_widget.urls')),
     url(r'^admin/grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/products/search', ProductSearchList.as_view(), name='product-search'),
+    url(r'^api/products/', ProductSearchList.as_view(), name='product-search'),
     url(r'^api/products/(?P<id>\d+)$', ProductDetail.as_view(), name='product-detail'),
     url(r'^', 'commerce.views.get_index', name='index'),
     url(r'^', 'commerce.views.get_index', name='index'),
-
-
 
 )
 
